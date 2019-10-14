9948f6bfb4c8b2fd423e90819f6f676e2a2f40c9
RubiMaistro <55796855+RubiMaistro@users.noreply.github.com>
2019-10-14 19:42:12 +0200

    Drill5

---
+++ drill5.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    /**
+    cout << "Success!\n"; //1
+
+    cout << "Success!\n"; //2
+
+    cout << "Success" << "!\n"; //3
+
+    cout << "success" << "!\n"; //4
+
+    int res = 7;       //5
+    vector<int> v(10);
+    v[5] = res;
+    cout << "Success!\n";
+
+    vector v(10);   //6
+    v(5) = 7;
+    if (v(5)==7)
+        cout << "Success!\n";
+
+    bool cond=true;     //7
+    if (cond) cout << "Success!\n";
+    else cout << "Fail!\n";
+
+    bool c = true;      //8
+    if (c) cout << "Success!\n";
+    else cout << "Fail!\n";
+
+    string s = "ape";   //9
+    bool c = "fool"<s;
+    if (!c) cout << "Success!\n";
+
+    string s = "ape";   //10
+    if (s=="ape") cout << "Success!\n";
+
+    string s = "ape";   //11
+    if (s!="fool" && s=="ape") cout < "Success!\n";
+
+    string s = "ape";   //12
+    if (s=="ape") cout < "Success!\n";
+
+    vector<char> v(5);  //13
+    for (int i=0; i<v.size(); ++i){
+    if(i=v.size()) cout << "Success!\n";
+    }
+
+    vector<char> v(5);  //14
+    for (int i=0; i<=v.size()-1; ++i){
+    if(i=v.size()) cout << "Success!\n";
+    }
+
+    string s = "Success!\n";    //15
+    for (int i=0; i<s.size(); ++i)
+        cout << s[i];
+
+    if (true)       //16
+        cout << "Success!\n";
+    else
+        cout << "Fail!\n";
+
+    int x = 2000;   //17
+    int c = x;
+    if (c==2000)
+        cout << "Success!\n";
+
+    string s = "Success!\n";    //18
+    for (int i=0; i<s.size(); ++i)
+        cout << s[i];
+
+    vector v(5);    //19
+    for (int i=0; i<=v.size()-1; ++i)
+    cout << "Success!\n";
+
+    int i = 0, j = 9;     //20
+    while (i<10) ++i;
+    if (j<i)
+        cout << "Success!\n";
+
+    int x = 2;  //21
+    double d = 5/x-2;
+    if (d==2/x+0.5)
+        cout << "Success!\n";
+
+    string<char> s = "Success!\n";  //22
+    for (int i=0; i<=s.size()-1; ++i)
+        cout << s[i];
+
+    int i=0, j=10;    //23
+    while (i<10) ++i;
+    if (j==i)
+        cout << "Success!\n";
+
+    int x = 4;  //24
+    double d = 5/(x-2);
+    if (d!=2*x+0.5)
+        cout << "Success!\n";
+
+    cout << "Success!\n";   //25 */
+return 0;
+}
